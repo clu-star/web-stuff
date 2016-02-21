418f902c503c25b5be54029f35a974899d7396a9
Jeffrey Souza <jeffreymsouza@gmail.com>
2016-02-21 00:15:59 -0500

    basic styling

--- check_extension.py
+++ check_extension.py
@@ -27,8 +27,13 @@ def upload_file():
                                     filename=filename))
     return '''
     <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
+    <head>
+    <link rel="stylesheet" type="text/css" href="/root/mhacks/web-stuff/mystyle.css">
+    </head>
+    <img src="/root/mhacks/web-stuff/oncoBOT.png" alt="oncoBOT">
+    <title>Upload New File</title>
+    <h1>Upload New File</h1>
+
     <form action="" method=post enctype=multipart/form-data>
       <p><input type=file name=file>
          <input type=submit value=Upload>
