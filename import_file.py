18359d9516c4f3dc5ea1a9cc91fa8ccfd1070f6f
Jeffrey Souza <jeffreymsouza@gmail.com>
2016-02-21 00:04:47 -0500

    init

---
+++ import_file.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/root/mhacks/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
